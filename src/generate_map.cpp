46a0b3926f64a7f7f2d22d4b601d7b81c3174caf
Nicholas Macrae Hodge <nhodge1@hydra23.eecs.utk.edu>
2020-03-11 22:01:06 -0400

    fixed formatting of generate_map.cpp

--- src/generate_map.cpp
+++ src/generate_map.cpp
@@ -25,12 +25,27 @@ int main(int argc, char *argv[]){
 	//read in the grid size from command line
 	int n = atoi(argv[1]);
 
-	char characters[6] = {'g', 'G', 'f', 'h', 'r', 'm'};
+	char characters[6] = {'f', 'g', 'G', 'h', 'm', 'r'};
+	int values[6] = {3, 1, 2, 4, 7, 5};
 
 	//open the file to write to
 	ofstream fout;
 	fout.open("map.txt");
 
+	//print out the number of characters
+	fout << "6\n";
+
+	//print out the characters and their values
+	for (int i = 0; i < 6; i++) {
+
+		fout << characters[i] << " " << values[i] << endl;
+
+	}
+
+	//print out the NxN grid size
+	fout << n << " " << n << endl;
+
+	//print out the map
 	for(int i = 0; i < n; i++){
 
 		for(int j = 0; j < n; j++){
@@ -42,6 +57,12 @@ int main(int argc, char *argv[]){
 		fout << endl;
 
 	}
+
+	//print out starting postion
+	fout << "0 0\n";
+
+	//print out the end positon
+	fout << n - 1 << " " << n - 1 << endl;
 
 	fout.close();
 
