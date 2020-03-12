bd38a2ce6120cf45c2b6529dc032c46050b22868
David <43688883+ksong200@users.noreply.github.com>
2020-03-11 23:41:37 -0400

    Update dijkstras.cpp

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -42,8 +42,8 @@ int main(int argc, char *argv[]) {
 	map<char,int>::iterator it;
 	for(int i = 0; i < numTile; i++){
 		
-		cin >> tilel >> tileCost;
-		cost[tileChar] = tileCost;
+		cin >> tilel>> tileCost;
+		cost[tilel] = tileCost;
 	}
 
 	int row;
@@ -65,11 +65,12 @@ int main(int argc, char *argv[]) {
 		for(int j = 0; j < col; j++){
 			cin >> tileChar;
 
-			grid[i][j].row = row;
-			grid[i][j].col = col;
+			grid[i][j].row = i;
+			grid[i][j].col = j;
 			
 			it = cost.find(tileChar);
 			grid[i][j].cost = it -> second;
+      grid[i][j].name = tileChar;
 		}
 
 	}
@@ -96,108 +97,108 @@ int main(int argc, char *argv[]) {
 	multimap<int, Node>::iterator mmIT;
 
 	nDistance.insert(make_pair(startN.distance, startN));
+
 	//Dijkstra's algorithm
 	while(nDistance.size() != 0){
-
 		mmIT = nDistance.begin();
 
 		
-
 		tempRow = mmIT->second.row;
 		tempCol = mmIT->second.col;
 
 		nDistance.erase(mmIT);
 
 		grid[tempRow][tempCol].visit = true;
-		
 		//check the right side
 		if(grid[tempRow][tempCol].col + 1 < col && grid[tempRow][tempCol+1].visit != true){
-			tempDistance = grid[tempRow][tempCol].distance + grid[tempRow][tempCol].cost;
+			tempDistance = grid[tempRow][tempCol].distance + grid[tempRow][tempCol+1].cost;
 			if(grid[tempRow][tempCol+1].distance == INT_MAX ||tempDistance<grid[tempRow][tempCol+1].distance){
-				for(mmIT = nDistance.begin(); mmIT != nDistance.end();mmIT++){
-					if(tempRow == mmIT->second.row && tempCol == mmIT ->second.col){
-						nDistance.erase(mmIT);
+				for(mmIT = nDistance.begin(); mmIT != nDistance.end(); mmIT++){
+					if(tempRow == mmIT->second.row && tempCol+1 == mmIT ->second.col){
+             //delete mmIT before iterating it
+						nDistance.erase(mmIT++);
+           }
            }
 						grid[tempRow][tempCol+1].distance = tempDistance;
 						grid[tempRow][tempCol+1].backRow = tempRow;
 						grid[tempRow][tempCol+1].backCol = tempCol;
-						nDistance.insert(make_pair(grid[tempRow][tempCol+1].distance, grid[tempRow][tempCol]));
-					
-				}
+						nDistance.insert(make_pair(grid[tempRow][tempCol+1].distance, grid[tempRow][tempCol+1]));				
 			}
 		}
 
 		//check the left side
    if(grid[tempRow][tempCol].col -1 >= 0 && grid[tempRow][tempCol-1].visit != true){
-			tempDistance = grid[tempRow][tempCol].distance + grid[tempRow][tempCol].cost;
+			tempDistance = grid[tempRow][tempCol].distance + grid[tempRow][tempCol-1].cost;
 			if(grid[tempRow][tempCol-1].distance == INT_MAX ||tempDistance<grid[tempRow][tempCol-1].distance){
-				for(mmIT = nDistance.begin(); mmIT != nDistance.end();mmIT++){
-					if(tempRow == mmIT->second.row && tempCol == mmIT ->second.col){
-						nDistance.erase(mmIT);
-                }                           
+				for(mmIT = nDistance.begin(); mmIT != nDistance.end();  mmIT++){
+					if(tempRow == mmIT->second.row && tempCol-1 == mmIT ->second.col){
+             //delete mmIT before iterating it
+						nDistance.erase(mmIT++);
+                }             
+        }      
 						grid[tempRow][tempCol-1].distance = tempDistance;
 						grid[tempRow][tempCol-1].backRow = tempRow;
 						grid[tempRow][tempCol-1].backCol = tempCol;
-						nDistance.insert(make_pair(grid[tempRow][tempCol-1].distance, grid[tempRow][tempCol]));
-					
-				}
-			}
+						nDistance.insert(make_pair(grid[tempRow][tempCol-1].distance, grid[tempRow][tempCol-1]));
+    	}
 		}
-		
+
    //check the top side
    if(grid[tempRow][tempCol].row - 1 >= 0 && grid[tempRow-1][tempCol].visit != true){
-			tempDistance = grid[tempRow][tempCol].distance + grid[tempRow][tempCol].cost;
+			tempDistance = grid[tempRow][tempCol].distance + grid[tempRow-1][tempCol].cost;
 			if(grid[tempRow-1][tempCol].distance == INT_MAX ||tempDistance<grid[tempRow-1][tempCol].distance){
-				for(mmIT = nDistance.begin(); mmIT != nDistance.end();mmIT++){
-					if(tempRow == mmIT->second.row && tempCol == mmIT ->second.col){
-						nDistance.erase(mmIT);
+				for(mmIT = nDistance.begin(); mmIT != nDistance.end(); mmIT++){
+					if(tempRow-1 == mmIT->second.row && tempCol == mmIT ->second.col){
+             //delete mmIT before iterating it
+						nDistance.erase(mmIT++);
             }
-						grid[tempRow-1][tempCol].distance = tempDistance;
-						grid[tempRow-1][tempCol].backRow = tempRow;
-						grid[tempRow-1][tempCol].backCol = tempCol;
-						nDistance.insert(make_pair(grid[tempRow-1][tempCol].distance, grid[tempRow][tempCol]));
-						
-				}
+        }
+				grid[tempRow-1][tempCol].distance = tempDistance;
+        grid[tempRow-1][tempCol].backRow = tempRow;
+        grid[tempRow-1][tempCol].backCol = tempCol;
+        nDistance.insert(make_pair(grid[tempRow-1][tempCol].distance, grid[tempRow-1][tempCol]));
 			}
 		}
-   
    //check the bottom side
-   
    if(grid[tempRow][tempCol].row + 1 < row && grid[tempRow+1][tempCol].visit != true){
-			tempDistance = grid[tempRow][tempCol].distance + grid[tempRow][tempCol].cost;
+			tempDistance = grid[tempRow][tempCol].distance + grid[tempRow+1][tempCol].cost;
 			if(grid[tempRow+1][tempCol].distance == INT_MAX ||tempDistance<grid[tempRow+1][tempCol].distance){
-				for(mmIT = nDistance.begin(); mmIT != nDistance.end();mmIT++){
-					if(tempRow == mmIT->second.row && tempCol == mmIT ->second.col){
-						nDistance.erase(mmIT);
+				for(mmIT = nDistance.begin(); mmIT != nDistance.end(); mmIT++){
+					if(tempRow+1 == mmIT->second.row && tempCol == mmIT ->second.col){
+            //delete mmIT before iterating it
+						nDistance.erase(mmIT++);
             }
+        }
 						grid[tempRow+1][tempCol].distance = tempDistance;
 						grid[tempRow+1][tempCol].backRow = tempRow;
 						grid[tempRow+1][tempCol].backCol = tempCol;
-						nDistance.insert(make_pair(grid[tempRow+1][tempCol].distance, grid[tempRow][tempCol]));
-						
-				}
+						nDistance.insert(make_pair(grid[tempRow+1][tempCol].distance, grid[tempRow+1][tempCol]));		
 			}
 		}
    
    }
    
    deque<int>path;
-   
-   cout << grid[rowEnd][colEnd].distance << endl;
+   int total_distance;
+   total_distance = grid[rowEnd][colEnd].distance - grid[rowEnd][colEnd].cost + grid[rowStart][colStart].cost;
+   cout << total_distance << endl;
    tempRow = rowEnd;
    tempCol = colEnd;
+   //loop backwards from the end of the path and push the path to the front of the deque
    while((grid[tempRow][tempCol].col != grid[rowStart][colStart].col) && (grid[tempRow][tempCol].row != grid[rowStart][colStart].row)){
        path.push_front(grid[tempRow][tempCol].backCol);
        path.push_front(grid[tempRow][tempCol].backRow);
+       tempRow = grid[tempRow][tempCol].backRow;
+       tempCol = grid[tempRow][tempCol].backCol;
    }
-   
-   for(int i = path.size(); i >= 0; i--){
+   cout << rowStart << " " << colStart << endl;
+   for(int i = 0; i < path.size(); i++){
        cout << path[i] << " ";
-       i--;
+       i++;
        cout << path[i] << endl;
    } 
   
-	
+	cout << rowEnd << " " << colEnd <<endl;
 	
 	return 0;
 }
